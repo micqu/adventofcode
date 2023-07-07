56ed6617aa1a13a9208e93a41af74408dfe805d5
micqu <mqf16@hotmail.com>
2023-07-07 23:09:00 +0200

    Refactored a bit

--- 2015/src/day24/mod.rs
+++ 2015/src/day24/mod.rs
@@ -3,42 +3,30 @@ use itertools::Itertools;
 use crate::utils;
 
 pub fn d24() {
-    d24_1();
-    d24_2();
-}
-
-pub fn d24_1() {
     let numbers = parse("src/day24/input.txt");
     let sum: u64 = numbers.iter().sum();
 
-    let result = solve(numbers, sum / 3);
+    let result = solve(&numbers, sum / 3);
+    println!("{}", result.unwrap());
+
+    let result = solve(&numbers, sum / 4);
     println!("{}", result.unwrap());
 }
 
-pub fn d24_2() {
-    let numbers = parse("src/day24/input.txt");
-    let sum: u64 = numbers.iter().sum();
-
-    let result = solve(numbers, sum / 4);
-    println!("{}", result.unwrap());
-}
-
-fn solve(v: Vec<u64>, target: u64) -> Option<u64> {
+fn solve(v: &Vec<u64>, target: u64) -> Option<u64> {
     for i in 2..=v.len() {
-        let mut groups = Vec::<Group>::new();
+        let mut groups_qe = Vec::<u64>::new();
 
-        for pair in v.iter().combinations(i) {
-            if pair.iter().map(|x| **x).sum::<u64>() == target {
-                groups.push(Group {
-                    numbers: pair.iter().map(|x| **x).collect_vec()
-                })
+        for pair in v.iter().copied().combinations(i) {
+            if pair.iter().sum::<u64>() == target {
+                groups_qe.push(pair.iter().product());
             }
         }
 
-        groups.sort_by_key(|x| x.calc_entanglement());
-
-        if let Some(s) = groups.first() {
-            return Some(s.calc_entanglement());
+        groups_qe.sort_unstable();
+        
+        if let Some(s) = groups_qe.first() {
+            return Some(*s);
         }
     }
     
@@ -55,17 +43,6 @@ fn parse(file: &str) -> Vec<u64> {
         let number = line.trim().parse().unwrap();
         numbers.push(number);
     }
-    numbers.reverse();
+    
     numbers
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct Group {
-    numbers: Vec<u64>
-}
-
-impl Group {
-    fn calc_entanglement(&self) -> u64 {
-        self.numbers.iter().product()
-    }
 }
--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -26,25 +26,25 @@ mod utils;
 fn main() {
     let start = SystemTime::now();
 
-    day7::d7();
-    day8::d8();
-    day9::d9();
-    day10::d10();
-    day11::d11();
-    day12::d12();
-    day13::d13();
-    day14::d14();
-    day15::d15();
-    day16::d16();
-    day17::d17();
-    day18::d18();
-    day19::d19();
-    day20::d20();
-    day21::d21();
-    day22::d22();
-    day23::d23();
+    // day7::d7();
+    // day8::d8();
+    // day9::d9();
+    // day10::d10();
+    // day11::d11();
+    // day12::d12();
+    // day13::d13();
+    // day14::d14();
+    // day15::d15();
+    // day16::d16();
+    // day17::d17();
+    // day18::d18();
+    // day19::d19();
+    // day20::d20();
+    // day21::d21();
+    // day22::d22();
+    // day23::d23();
     day24::d24();
-    day25::d25();
+    // day25::d25();
 
     println!("\nTotal time: {:?}", start.elapsed().unwrap());
 }
