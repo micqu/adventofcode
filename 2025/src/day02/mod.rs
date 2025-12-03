cccd2dfeb05c3794dd2a55df98bba58a7eb184cb
micqu <mqf16@hotmail.com>
2025-12-03 10:28:47 +0100

    .

--- 2025/src/day02/mod.rs
+++ 2025/src/day02/mod.rs
@@ -1,6 +1,11 @@
-use crate::utils::{solution::{IntoSolution, Solution}, Parsable};
+use num::integer::Roots;
 
-pub const TITLE: &str = "";
+use crate::utils::{
+    solution::{IntoSolution, Solution},
+    Parsable,
+};
+
+pub const TITLE: &str = "Gift Shop";
 const INPUT: &'static str = include_str!("input.txt");
 
 pub fn part1() -> Option<Solution> {
@@ -8,20 +13,69 @@ pub fn part1() -> Option<Solution> {
 }
 
 pub fn part2() -> Option<Solution> {
-    None
+    let mut ids = 0;
+    let ranges = parse();
+
+    for Range { from, to } in ranges {
+        for n in from..=to {
+            let len = length(n);
+
+            for i in 1..=len / 2 {
+                if len % i == 0 {
+                    let o = 10_u32.pow(i - 1);
+                    let k = mirror(o, i, len / i);
+                    if n % (k / o) as usize == 0 {
+                        ids += n;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    ids.solution()
+}
+
+fn length(a: usize) -> u32 {
+    a.checked_ilog10().unwrap_or(0) + 1
+}
+
+fn mirror(a: u32, len: u32, n: u32) -> u32 {
+    let mut r = 0;
+    for _ in 0..n {
+        r = r * 10_u32.pow(len) + a;
+    }
+    r
+}
+
+fn parse() -> Vec<Range> {
+    let mut ranges = Vec::new();
+    let mut bytes = INPUT.bytes();
+    while let Some(a) = Parsable::<usize>::next_number(&mut bytes) {
+        let b = bytes.next_number().unwrap();
+        ranges.push(Range { from: a, to: b });
+    }
+
+    ranges
+}
+
+#[derive(Debug)]
+struct Range {
+    from: usize,
+    to: usize,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn part1() {
-        assert_eq!(super::part1(), (2196996 as u32).solution());
+        assert_eq!(super::part1(), (5398419778 as usize).solution());
     }
 
     #[test]
     fn part2() {
-        assert_eq!(super::part2(), (23655822 as u32).solution());
+        assert_eq!(super::part2(), (15704845910 as usize).solution());
     }
 }
--- 2025/src/day03/mod.rs
+++ 2025/src/day03/mod.rs
@@ -1,27 +1,63 @@
-use crate::utils::{solution::{IntoSolution, Solution}, Parsable};
+use crate::utils::{
+    solution::{IntoSolution, Solution},
+    Parsable,
+};
 
-pub const TITLE: &str = "";
+pub const TITLE: &str = "Lobby";
 const INPUT: &'static str = include_str!("input.txt");
 
 pub fn part1() -> Option<Solution> {
-    None
+    INPUT
+        .lines()
+        .filter_map(|line| solve(line, 0, 2))
+        .sum::<usize>()
+        .solution()
 }
 
 pub fn part2() -> Option<Solution> {
-    None
+    INPUT
+        .lines()
+        .filter_map(|line| solve(line, 0, 12))
+        .sum::<usize>()
+        .solution()
+}
+
+fn solve(line: &str, pos: usize, rem: usize) -> Option<usize> {
+    if rem == 0 {
+        return None;
+    }
+
+    let mut bytes = line.bytes().skip(pos);
+    let mut max = 0;
+    let mut next = 0;
+
+    for i in pos..line.len() - rem + 1 {
+        let a = bytes.next().unwrap();
+        if a > max {
+            max = a;
+            next = i;
+        }
+    }
+
+    if let Some(s) = solve(line, next + 1, rem - 1) {
+        let l = s.checked_ilog10().unwrap_or(0) + 1;
+        Some((max - b'0') as usize * 10_usize.pow(l) + s)
+    } else {
+        Some((max - b'0') as usize)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn part1() {
-        assert_eq!(super::part1(), (2196996 as u32).solution());
+        assert_eq!(super::part1(), (17412 as usize).solution());
     }
 
     #[test]
     fn part2() {
-        assert_eq!(super::part2(), (23655822 as u32).solution());
+        assert_eq!(super::part2(), (172681562473501 as usize).solution());
     }
 }
